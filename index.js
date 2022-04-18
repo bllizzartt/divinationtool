90b944cfb5280aa839ca56b09bf71cdf92fed382
Chase_Haynes <chase.haynez@gmail.com>
2022-04-17 23:56:38 -0400

    button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let btn = document.createElement("button");
 btn.innerHTML = "Click Me";
 document.body.appendChild(btn);
+
+<button onclick="getElementById('demo').innerHTML = Date()">What is the time?</button>
