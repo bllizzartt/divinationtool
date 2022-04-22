a3cd8fdcfa8f1a8ddd73b5b47afda4e20eecfe26
Chase_Haynes <chase.haynez@gmail.com>
2022-04-21 23:00:04 -0400

    color setup

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-// const btn = document.getElementById('btn');
 
-// btn.addEventListener('click', function onClick(event) {
-//   // 👇️ change background color
-//   document.body.style.backgroundColor = 'red', 'yellow', 'green';
+const btn = document.getElementById('btn');
 
-//   // 👇️ optionally change text color
-//   // document.body.style.color = 'white';
-// });
+btn.addEventListener('click', function onClick(event) {
+  // 👇️ change background color
+  document.body.style.backgroundColor = 'red', 'yellow', 'green';
+  var randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber == 0){
+    document.body.style.backgroundColor = 'red';
+  }
+  else if (randomNumber == 1) {
+    document.body.style.backgroundColor = 'yellow';
+  }
+
+  else if (randomNumber == 2) {
+    document.body.style.backgroundColor = 'green';
+  }
+  // 👇️ optionally change text color
+   //document.body.style.color = 'black';
+});
 
 // function changeColor(){
 //     document.body.style.backgroundColor= 'rgb(' + Math.round('red') + ',' + Math.round('yellow') + ',' Math.round('yellow');
-// }
+// });
 
